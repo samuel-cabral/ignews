0a631bed412e6b8ff6012fde463555cb6c1a7526
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-10-11 21:58:04 -0300

    ✨ feat: finalizado o component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src="/images/logo.svg" width="110" height="31" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
