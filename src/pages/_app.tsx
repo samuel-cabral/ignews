460bf91ff78070bcb8de4696d2584d7d62d04044
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-06-04 00:41:49 -0300

    [API Routes no Next.js] Autenticacao com Next Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react'
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthSessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthSessionProvider>
   );
 }
 
