f4f0b3f327b8bbb9d33c199619b882ee67d17ee1
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-10-05 22:14:20 -0300

    Config prismic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,30 @@
+import Link from 'next/link';
+
+import { PrismicProvider } from '@prismicio/react';
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName } from '../../prismicio';
+
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
-import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react'
+import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthSessionProvider session={pageProps.session}>
-      <Header />
-      <Component {...pageProps} />
+      <PrismicProvider
+        internalLinkComponent={({ href, ...props }) => (
+          <Link href={href}>
+            <a {...props} />
+          </Link>
+        )}
+      >
+        <PrismicPreview repositoryName={repositoryName}>
+          <Header />
+          <Component {...pageProps} />
+        </PrismicPreview>
+      </PrismicProvider>
     </NextAuthSessionProvider>
   );
 }
