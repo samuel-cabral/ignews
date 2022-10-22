5da65bf64b7baa3e8cdf378de0ccddef072b35a7
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-10-22 00:49:32 -0300

    Configurando API do prismic

--- src/pages/slice-simulator.tsx
+++ src/pages/slice-simulator.tsx
@@ -1,15 +1,13 @@
 import { SliceSimulator } from '@prismicio/slice-simulator-react';
 import { SliceZone } from '@prismicio/react';
 
-import { components } from '../slices';
-import state from '../.slicemachine/libraries-state.json';
+import { components } from '../../slices';
+import state from '../../.slicemachine/libraries-state.json';
 
 const SliceSimulatorPage = () => {
   return (
     <SliceSimulator
-      sliceZone={({ slices }) => (
-        <SliceZone slices={slices} components={components} />
-      )}
+      sliceZone={props => <SliceZone {...props} components={components} />}
       state={state}
     />
   );
---
+++ src/services/prismic.ts
@@ -0,0 +1,11 @@
+import { ClientConfig, createClient } from "@prismicio/client";
+
+export function getPrismicClient(options?: ClientConfig) {
+  return createClient(
+    process.env.PRISMIC_ENDPOINT,
+    {
+      ...options,
+      accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    }
+  )
+}
